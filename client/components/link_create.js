f7400e573469d52cbf3ec049a914cba6f2c914d9
Chetan Sanghani <crsanghani@gmail.com>
2017-01-28 01:11:12 +0000

    event handler and form ref written for link_create component

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
 class LinkCreate extends Component {
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.refs.link.value);
+
+  }
+
   render () {
     return(
-      <form action=''>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className='form-group'>
-          <label htmlFor=''>Link to shorten</label>
-          <input type='text' className='form-control'/>
+          <label>Link to shorten</label>
+          <input ref='link' type='text' className='form-control'/>
         </div>
         <button className='btn btn-primary'>Shorten!</button>
       </form>
