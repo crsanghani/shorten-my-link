b3427bdc1baf650b02153a30d53f63f448c51628
Chetan Sanghani <crsanghani@gmail.com>
2017-01-29 22:07:23 +0000

    added a middleware to check if a user is attempting to visit a shortened link

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -8,3 +10,9 @@ Meteor.startup(() => {
     return Links.find({});
   });
 });
+
+const middleware = ConnectRoute(function(router){
+  router.get('/:token', (req) => console.log(req))
+});
+
+WebApp.connectHandlers.use(middleware);
