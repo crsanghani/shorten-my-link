51ed4bcb2ba718601cf1647f7fc6907ad2a8b83f
Chetan Sanghani <crsanghani@gmail.com>
2017-01-29 21:38:40 +0000

    wrote token generation and catting of link as well as display of generated links

--- server/main.js
+++ server/main.js
@@ -3,4 +3,8 @@ import { Links } from '../imports/collections/links';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Meteor.publish('links', function() {
+    return Links.find({});
+  });
 });
