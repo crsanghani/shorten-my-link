fbf88a35210dfdea653810d34aa6917afe2a9bf3
Chetan Sanghani <crsanghani@gmail.com>
2017-01-29 22:18:20 +0000

    when a user visits a shorten link from anywhere only meteor responds now, it doesnt touch our react app

--- server/main.js
+++ server/main.js
@@ -11,8 +11,32 @@ Meteor.startup(() => {
   });
 });
 
+// exectuted when a user visits with a route like
+// 'localhost:3000/ABCD'
+
+function onRoute(req, res, next) {
+  // take the token out of the url
+  // find a matching link in the links collection
+  const link = Links.findOne({ token: req.params.token });
+
+  if ( link ) {
+    // if we find a link object, redirect the user
+    // to the long url
+
+    res.writeHead(307, { 'Location': link.url});
+    res.end();
+
+  } else {
+    // if we dont find a link object, send the user to
+    // our nuormal React App
+
+    next();
+
+  }
+}
+
 const middleware = ConnectRoute(function(router){
-  router.get('/:token', (req) => console.log(req))
+  router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
